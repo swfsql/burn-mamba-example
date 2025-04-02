f889743a97a3b0882b570a109a150745a2bc412b
swfsql <swfsql@users.noreply.github.com>
2025-04-02 02:22:08 +0000

    deploy: 818a754ca63017e2facc4e30c7579e448ca0aee9

--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-import init, { wasm_main } from "./pkg/burn_mamba_example.js";
-
-async function run() {
-    await init();
-    wasm_main();
-}
-run();
--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod common;
-
-#[cfg(feature = "native")]
-pub mod native;
-
-#[cfg(target_arch = "wasm32")]
-pub mod wasm;
-
-pub use common::*;
